2346fd9b3d4c1359bcfcd4acf55f4d412d6cb91d
penso996 <penso.andrea@outlook.it>
2025-05-05 15:49:08 +0200

    added index and js, done ex1

---
+++ script.js
@@ -0,0 +1,23 @@
+// SNACK 1
+// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
+// Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+// Funzione Dichiarativa
+function sommaDichiarativa(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(sommaDichiarativa(3, 5));
+
+// Funzione Anonima
+const sommaAnonima = function (numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(sommaAnonima(2, 4));
+
+// Arrow Function
+const sommaArrow = (numero1, numero2) => numero1 + numero2;
+
+console.log(sommaArrow(1, 2));
