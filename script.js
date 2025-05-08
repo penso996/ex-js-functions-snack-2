b871cd7f47dd01a1f2a7ae981dc0bdc5339b9ea4
penso996 <penso.andrea@outlook.it>
2025-05-08 12:41:27 +0200

    code question 1 and 2 done

--- script.js
+++ script.js
@@ -1,158 +1,210 @@
-// SNACK 1
-// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
-// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
-// Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+// // SNACK 1
+// // Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+// // Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile.
+// // Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
 
-// Funzione Dichiarativa
-console.log(sommaDichiarativa(3, 5));
+// // Funzione Dichiarativa
+// console.log(sommaDichiarativa(3, 5));
 
-function sommaDichiarativa(numero1, numero2) {
-    return numero1 + numero2;
-}
+// function sommaDichiarativa(numero1, numero2) {
+//     return numero1 + numero2;
+// }
 
-// Funzione Anonima
-const sommaAnonima = function (numero1, numero2) {
-    return numero1 + numero2;
-}
+// // Funzione Anonima
+// const sommaAnonima = function (numero1, numero2) {
+//     return numero1 + numero2;
+// }
 
-console.log(sommaAnonima(2, 4));
+// console.log(sommaAnonima(2, 4));
 
-// Arrow Function
-const sommaArrow = (numero1, numero2) => numero1 + numero2;
+// // Arrow Function
+// const sommaArrow = (numero1, numero2) => numero1 + numero2;
 
-console.log(sommaArrow(1, 2));
+// console.log(sommaArrow(1, 2));
 
-// SNACK 2
-// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
-const quadrato = (numero) => numero * numero;
 
-console.log(quadrato(5));
 
-// SNACK 3
-// Definisci una funzione eseguiOperazione che accetta tre parametri:
-// due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
-const sottrazione = (numero1, numero2) => numero1 - numero2;
-const resto = (numero1, numero2) => numero1 % numero2;
+// // SNACK 2
+// // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+// const quadrato = (numero) => numero * numero;
 
-const eseguiOperazione = (numero1, numero2, operazione) => {
-    return operazione(numero1, numero2);
-}
+// console.log(quadrato(5));
 
-console.log(eseguiOperazione(4, 2, sottrazione));
-console.log(eseguiOperazione(5, 2, resto));
 
-// SNACK 4
-// Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
-const creaTimer = (tempo) => {
-    setTimeout(() => {
-        console.log("Tempo scaduto!")
-    }, tempo)
-}
 
-creaTimer(1000);
+// // SNACK 3
+// // Definisci una funzione eseguiOperazione che accetta tre parametri:
+// // due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+// const sottrazione = (numero1, numero2) => numero1 - numero2;
+// const resto = (numero1, numero2) => numero1 % numero2;
 
-// SNACK 5
-// Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
-// Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
-const messaggioTemporizzato = (messaggio) => {
-    setInterval(() => {
-        console.log(messaggio)
-    }, 1000)
-}
+// const eseguiOperazione = (numero1, numero2, operazione) => {
+//     return operazione(numero1, numero2);
+// }
 
-messaggioTemporizzato("Snack 5");
+// console.log(eseguiOperazione(4, 2, sottrazione));
+// console.log(eseguiOperazione(5, 2, resto));
 
-// SNACK 6
-// Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo
-// e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
-const creaContatoreAutomatico = (tempo) => {
-    let contatore = 0;
-    const id = setInterval(() => {
-        if (contatore < 3) {    // Stoppa dopo 3 iterazioni
-            contatore++
-            console.log(contatore)
-        } else {
-            clearInterval(id)
-        }
-    }, tempo)
-}
 
-creaContatoreAutomatico(1000);
 
-// SNACK 7
-// Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop.
-// Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
-const eseguiEferma = (messaggio, intervallo, tempoStop) => {
-    let tempoAttuale = 0;
-    const id = setInterval(() => {
-        if (tempoAttuale < tempoStop) {
-            console.log(messaggio)
-            tempoAttuale += 1000
-        } else {
-            clearInterval(id)
-        }
-    }, intervallo)
-}
+// // SNACK 4
+// // Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+// const creaTimer = (tempo) => {
+//     setTimeout(() => {
+//         console.log("Tempo scaduto!")
+//     }, tempo)
+// }
 
-eseguiEferma("Snack 7", 1000, 3000);
+// creaTimer(1000);
 
-// SNACK 8
-// Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero.
-// Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.
-const contoAllaRovescia = (numero) => {
-    const id = setInterval(() => {
-        if (numero > 0) {
-            console.log(numero)
-            numero--;
-        } else {
-            clearInterval(id)
-            console.log("Tempo scaduto!")
-        }
-    }, 1000)
-}
 
-contoAllaRovescia(5);
 
-// SNACK 9
-// Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
-// Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
-const sequenzaOperazioni = (operazioni, intervallo) => {
-    operazioni.forEach((operazione, indice) => {
-        setTimeout(() => {
-            operazione();
-        }, intervallo * indice)
-    })
-}
+// // SNACK 5
+// // Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+// // Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+// const messaggioTemporizzato = (messaggio) => {
+//     setInterval(() => {
+//         console.log(messaggio)
+//     }, 1000)
+// }
 
-const operazioni = [
-    () => console.log("Operazione 1"),
-    () => console.log("Operazione 2"),
-    () => console.log("Operazione 3")
-];
+// messaggioTemporizzato("Snack 5");
 
-sequenzaOperazioni(operazioni, 1000);
 
-// SNACK 10
-// Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
-// Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
-function creaThrottler(callback, limite) {
 
-    let ultimaEsecuzione = 0;
+// // SNACK 6
+// // Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo
+// // e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+// const creaContatoreAutomatico = (tempo) => {
+//     let contatore = 0;
+//     const id = setInterval(() => {
+//         if (contatore < 3) {    // Stoppa dopo 3 iterazioni
+//             contatore++
+//             console.log(contatore)
+//         } else {
+//             clearInterval(id)
+//         }
+//     }, tempo)
+// }
 
-    return function (...args) {
+// creaContatoreAutomatico(1000);
 
-        const ora = Date.now();
 
-        if (ora - ultimaEsecuzione >= limite) {
-            ultimaEsecuzione = ora;
-            callback(...args);
-        }
-    }
-}
 
-const throttledLog = creaThrottler(() => console.log
-    ("Eseguito!"), 2000)
+// // SNACK 7
+// // Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop.
+// // Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+// const eseguiEferma = (messaggio, intervallo, tempoStop) => {
+//     let tempoAttuale = 0;
+//     const id = setInterval(() => {
+//         if (tempoAttuale < tempoStop) {
+//             console.log(messaggio)
+//             tempoAttuale += 1000
+//         } else {
+//             clearInterval(id)
+//         }
+//     }, intervallo)
+// }
 
-throttledLog();
-throttledLog();
-setTimeout(throttledLog, 2500);
+// eseguiEferma("Snack 7", 1000, 3000);
+
+
+
+// // SNACK 8
+// // Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero.
+// // Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.
+// const contoAllaRovescia = (numero) => {
+//     const id = setInterval(() => {
+//         if (numero > 0) {
+//             console.log(numero)
+//             numero--;
+//         } else {
+//             clearInterval(id)
+//             console.log("Tempo scaduto!")
+//         }
+//     }, 1000)
+// }
+
+// contoAllaRovescia(5);
+
+
+
+// // SNACK 9
+// // Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+// // Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+// const sequenzaOperazioni = (operazioni, intervallo) => {
+//     operazioni.forEach((operazione, indice) => {
+//         setTimeout(() => {
+//             operazione();
+//         }, intervallo * indice)
+//     })
+// }
+
+// const operazioni = [
+//     () => console.log("Operazione 1"),
+//     () => console.log("Operazione 2"),
+//     () => console.log("Operazione 3")
+// ];
+
+// sequenzaOperazioni(operazioni, 1000);
+
+
+
+// // SNACK 10
+// // Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
+// // Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
+// function creaThrottler(callback, limite) {
+
+//     let ultimaEsecuzione = 0;
+
+//     return function (...args) {
+
+//         const ora = Date.now();
+
+//         if (ora - ultimaEsecuzione >= limite) {
+//             ultimaEsecuzione = ora;
+//             callback(...args);
+//         }
+//     }
+// }
+
+// const throttledLog = creaThrottler(() => console.log
+//     ("Eseguito!"), 2000)
+
+// throttledLog();
+// throttledLog();
+// setTimeout(throttledLog, 2500);
+
+
+
+// // CODE QUESTION 1
+// const hamburger = { name: "Cheese Burger", weight: 250 };
+// const secondBurger = hamburger;
+// secondBurger.name = 'Double Cheese Burger';
+// secondBurger.weight = 500;
+
+// console.log(hamburger.name); // Double Cheese Burger
+// console.log(secondBurger.name); // Double Cheese Burger
+
+// // Viene creato un solo  oggetto in memoria
+
+
+
+// // CODE QUESTION 2
+// const hamburger = {
+// 	name: "Cheese Burger",
+// 	weight: 250,
+// 	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+// };
+
+// const secondBurger = {...hamburger};
+// secondBurger.ingredients[0] = "Salad";
+
+// console.log(hamburger.ingredients[0]); // Salad
+// console.log(secondBurger.ingredients[0]); // Salad
+
+// // Vengono creati 3 oggetti in memoria
+
+
+
+// CODE QUESTION 3
