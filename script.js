756523083866f1b87d9a9f2b6ed5d96853d2a9cc
penso996 <penso.andrea@outlook.it>
2025-05-08 12:24:51 +0200

    snack 10 done

--- script.js
+++ script.js
@@ -135,3 +135,24 @@ sequenzaOperazioni(operazioni, 1000);
 // SNACK 10
 // Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
 // Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
+function creaThrottler(callback, limite) {
+
+    let ultimaEsecuzione = 0;
+
+    return function (...args) {
+
+        const ora = Date.now();
+
+        if (ora - ultimaEsecuzione >= limite) {
+            ultimaEsecuzione = ora;
+            callback(...args);
+        }
+    }
+}
+
+const throttledLog = creaThrottler(() => console.log
+    ("Eseguito!"), 2000)
+
+throttledLog();
+throttledLog();
+setTimeout(throttledLog, 2500);
